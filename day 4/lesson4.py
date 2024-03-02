39e9d842f2333f049fd098a91d68aa142aeeeba6
Nickoqro1 <oqropiridzen10@gmail.com>
2024-03-03 00:34:23 +0400

    fourth commit

---
+++ day 4/lesson4.py
@@ -0,0 +1,9 @@
+name = input("what is your name: ")
+
+
+age = input("what is your age: ")
+
+
+lastname = input("what is your lastname: ")
+
+print("your name: " + name + " :your age: " + age + " :your lastname: " + lastname)
